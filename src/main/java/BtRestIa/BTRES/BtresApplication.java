aba0bb4dfef3208a3081034a56428fc6fccaf95f
Abdel201422 <abouadra@emeal.nttdata.com>
2025-04-11 12:27:08 +0200

    Crear proyecto

---
+++ src/main/java/BtRestIa/BTRES/BtresApplication.java
@@ -0,0 +1,13 @@
+package BtRestIa.BTRES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtresApplication.class, args);
+	}
+
+}
