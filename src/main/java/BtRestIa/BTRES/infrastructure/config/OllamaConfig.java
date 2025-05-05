cd43eb4a964d900e1b408ef70d9388b8d1ecb91b
Abdel201422 <abouadra@emeal.nttdata.com>
2025-05-05 13:08:30 +0200

    Actualizar consultaService

--- src/main/java/BtRestIa/BTRES/infrastructure/config/OllamaConfig.java
+++ src/main/java/BtRestIa/BTRES/infrastructure/config/OllamaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ai.ollama.OllamaChatModel;
-import BtRestIa.BTRES.infrastructure.dto.request.PreguntaRequestDto; // Asegúrate de importar la clase correcta
+import BtRestIa.BTRES.infrastructure.dto.request.PreguntaRequestDto;
 
 @Configuration
 public class OllamaConfig {
@@ -18,13 +18,16 @@ public class OllamaConfig {
     @Bean
     public OllamaChatModel.Builder ollamaModelBuilder(OllamaApi ollamaApi) {
         return OllamaChatModel.builder()
-                .ollamaApi(ollamaApi)
+                .ollamaApi(ollamaApi);
+    }
+    // Para más detalles sobre las opciones de configuración, puedes consultar la documentación oficial de Ollama: https://github.com/ollama/ollama/blob/main/docs/api.md
+    public OllamaChatModel getOllamaModel(PreguntaRequestDto preguntaRequestDto) {
+        return ollamaModelBuilder(ollamaApi())
                 .defaultOptions(
                         OllamaOptions.builder()
-                                .model("llama3:8b")
-                                .temperature(0.3)
-                                // .format("json")
+                                .model(preguntaRequestDto.getModelo())
+                                .temperature(0.3)  // Temperature controla la aleatoriedad de las respuestas generadas, Valores más bajos hacen que las respuestas sean más deterministas.
                                 .build()
-                );
+                ).build();
     }
 }
