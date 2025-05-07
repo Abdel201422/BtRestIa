23b68238823d68474b2432ee23766385d5d2e325
Abdel201422 <abouadra@emeal.nttdata.com>
2025-05-07 09:00:52 +0200

    Test

--- src/main/java/BtRestIa/BTRES/domain/ModeloIA.java
+++ src/main/java/BtRestIa/BTRES/domain/ModeloIA.java
@@ -1,8 +1,16 @@
 package BtRestIa.BTRES.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "modelo_ia")
 public class ModeloIA {
     @Id
@@ -12,44 +20,4 @@ public class ModeloIA {
     private String descripcion;
     private boolean activo;
 
-    public ModeloIA() {
-    }
-
-    public ModeloIA(Long id, String nombre, String descripcion, boolean activo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.activo = activo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) { this.activo = activo;
-    }
 }
--- src/main/java/BtRestIa/BTRES/domain/Usuario.java
+++ src/main/java/BtRestIa/BTRES/domain/Usuario.java
@@ -3,8 +3,16 @@ package BtRestIa.BTRES.domain;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "usuario")
 public class Usuario {
 
@@ -16,56 +24,5 @@ public class Usuario {
     private String email;
     @Column(name = "fecha_creacion")
     private LocalDateTime fechaCreacion = LocalDateTime.now();
-
-    public Usuario() {
-    }
-
-    public Usuario(Long id, String token, String nombre, String email, LocalDateTime fechaCreacion) {
-        this.id = id;
-        this.token = token;
-        this.nombre = nombre;
-        this.email = email;
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
 
 }
